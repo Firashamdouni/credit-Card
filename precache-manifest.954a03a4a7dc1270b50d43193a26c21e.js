6a9ec2b14dc52490796762e035faa61b3eed86a8
159753ess <firashamdouni3@gmail.com>
2018-11-22 09:50:56 +0100

    Updates

---
+++ precache-manifest.954a03a4a7dc1270b50d43193a26c21e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7782aaac7bf21fc836d55d7cae4045ec",
+    "url": "/credit-Card/static/media/puce0.7782aaac.png"
+  },
+  {
+    "revision": "d3c3fb698ce7671bea7eb8acc9c27bfa",
+    "url": "/credit-Card/static/media/master.d3c3fb69.PNG"
+  },
+  {
+    "revision": "1ff568be0b9d473a4d6669dfc0e6b80e",
+    "url": "/credit-Card/static/media/card-back.1ff568be.jpg"
+  },
+  {
+    "revision": "cd391217a74fd7637d77ec90876159cf",
+    "url": "/credit-Card/static/media/back.cd391217.jpg"
+  },
+  {
+    "revision": "ce852219f9bf68a72495",
+    "url": "/credit-Card/static/js/runtime~main.ce852219.js"
+  },
+  {
+    "revision": "82ca054b070550eb3940",
+    "url": "/credit-Card/static/js/main.82ca054b.chunk.js"
+  },
+  {
+    "revision": "5ce46033fa6b69d68eed",
+    "url": "/credit-Card/static/js/1.5ce46033.chunk.js"
+  },
+  {
+    "revision": "82ca054b070550eb3940",
+    "url": "/credit-Card/static/css/main.38bd40ae.chunk.css"
+  },
+  {
+    "revision": "5ce46033fa6b69d68eed",
+    "url": "/credit-Card/static/css/1.281d48d0.chunk.css"
+  },
+  {
+    "revision": "f9682ec37019e84aa21b1958f5d8e069",
+    "url": "/credit-Card/index.html"
+  }
+];
